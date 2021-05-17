04b91be18cffc85b173aa4f162d88bfff2bbd7ae
Drin Musa <drin9915@gmail.com>
2021-05-17 19:16:52 +0200

    Bug fix in contact and save messages locally

--- mail.php
+++ mail.php
@@ -1,12 +1,11 @@
 <?php
-
-if (!strlen(trim($_POST['name'])) && !strlen(trim($_POST['email'])) && !strlen(trim($_POST['phone'])) && !strlen(trim($_POST['message'])) ){
+if (strlen(trim($_POST['name']))!=0 || strlen(trim($_POST['email']))!=0 || strlen(trim($_POST['phone']))!=0 || strlen(trim($_POST['message']))!=0)
+	{
 	$name=$_POST['name']; 
 
 	$email=$_POST['email'];
 	$phone=$_POST['phone'];
 	$message=$_POST['message'];
-	
 
 	include('PHPMailer/PHPMailerAutoload.php');
 	$mail=new PHPMailer(true);
@@ -28,33 +27,34 @@ if (!strlen(trim($_POST['name'])) && !strlen(trim($_POST['email'])) && !strlen(t
 		'allow_self_signed'=>false
 	));
     
-    $alert = '';
+  
 
-    if($mail->send()){
-        echo '<script> alert("Your message was sent someone from our team will contact you as soon as possible")</script>';
-				echo '<script> window.location.href ="contact.php" </script>';
-				try{
-					$filename = 'messages.txt';
-					if(!file_exists($filename)){
-						throw new Exception('File not found');
-					}
-					$myfile = fopen($filename, "w") 
-					$txt = "From ".$email."\n"."Message: ".$message."\n"."================="."\n";
-					fwrite($myfile, $txt);
-					fclose($myfile);
+   	 if($mail->send()){
+			try{
+				$filename = 'messages.txt';
+				if(!file_exists($filename)){
+					throw new Exception('File not found');
 				}
-				catch(Exception $e){
-					echo "Messages failed to save: ".$e;
-				}
-		}
-                    else {
-											echo '<script> alert("There was an error,please try again")</script>';
-											echo'<script> window.location.href = "contact.php"</script>';
-?>
-										}
+				$myfile = fopen($filename, "w");
+				$txt = "From ".$email."\n"."Message: ".$message."\n"."================="."\n";
+				fwrite($myfile, $txt);
+				fclose($myfile);
+			}
+			catch(Exception $e){
+				echo "Messages failed to save: ".$e;
+			}
+        	echo '<script> alert("Your message was sent someone from our team will contact you as soon as possible")</script>';
+					echo '<script> window.location.href ="contact.php" </script>';
+			}
+                   		 else  {
+													echo '<script> alert("There was an error,please try again")</script>';
+													echo'<script> window.location.href = "contact.php"</script>';
+											}
 
 	
 	}
+
+	
 	else{
 		echo '<script> alert("Fields marked with * icon are required")</script>';
 		echo'<script> window.location.href = "contact.php"</script>';
